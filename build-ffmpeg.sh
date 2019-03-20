afa9a7cd23470ecb9b94c851ff1c50d43ab4f0bc
Gabriel Wu <qqbbnease1004@126.com>
2019-03-20 11:10:02 +0800

    v 0.0.10

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+cd nv-codec-headers
+make
+make install
+cd ..
+
 git clone https://github.com/ffmpeg/ffmpeg.git
 cd ffmpeg
 ./configure --enable-nonfree --disable-shared --enable-nvenc --enable-cuda --enable-cuvid --enable-libnpp --extra-cflags=-Ilocal/include --enable-gpl --enable-version3 --enable-libpulse --disable-debug --disable-ffplay --disable-indev=sndio --disable-outdev=sndio --enable-fontconfig --enable-frei0r --enable-gnutls --enable-gray --enable-libass --enable-libfreetype --enable-libfribidi --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libopenjpeg --enable-libopus --enable-librtmp --enable-libsoxr --enable-libspeex --enable-libtheora --enable-libvo-amrwbenc --enable-libvorbis --enable-libvpx --enable-libwebp --enable-libx264 --enable-libx265 --enable-libxvid --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64
